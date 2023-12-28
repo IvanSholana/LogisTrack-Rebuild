a2e35ee83d50bb9bb0bc6fb2254e449523c6f02d
Ivan Sholana <ivansholana@gmail.com>
2023-12-28 22:25:54 +0700

    feat : add profile screen

--- containers/Profile/UserProfileContainer.js
+++ containers/Profile/UserProfileContainer.js
@@ -1,5 +1,60 @@
-const userProfileContainer = () => {
-  return <></>;
+import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
+import { colors } from "../../constants/colors";
+
+const UserProfileContainer = () => {
+  const userdata = useSelector((state) => state.login);
+  console.log(userdata);
+  return (
+    <>
+      <Text style={styles.label}>Nama Lengkap</Text>
+      <Text style={styles.content}>{userdata.name}</Text>
+      {userdata.status == "Dosen" ? (
+        <Text style={styles.label}>NIDN</Text>
+      ) : (
+        <Text style={styles.label}>NIM</Text>
+      )}
+      <Text style={styles.content}>{userdata.nimNidn}</Text>
+      <Text style={styles.label}>Status</Text>
+      <Text style={styles.content}>{userdata.status}</Text>
+    </>
+  );
 };
 
-export default userProfileContainer;
+const UserProfileAppBar = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Profile Account</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30,
+    paddingBottom: 10,
+    backgroundColor: "#fff",
+    overflow: "hidden",
+    borderBottomColor: "#D9D9D9",
+    borderBottomWidth: 1,
+  },
+  headerText: {
+    fontSize: 24,
+    color: "#6A994E",
+    textAlignVertical: "center",
+    paddingLeft: 20,
+    backgroundColor: colors.loginText,
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 5,
+  },
+  content: {
+    fontSize: 15,
+  },
+});
+
+export { UserProfileContainer, UserProfileAppBar };
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,27 @@
+import { StyleSheet, View } from "react-native";
+import {
+  UserProfileAppBar,
+  UserProfileContainer,
+} from "../containers/Profile/UserProfileContainer";
+
 const ProfileScreen = () => {
-  return <></>;
+  return (
+    <>
+      <View>
+        <UserProfileAppBar />
+        <View style={styles.content}>
+          <UserProfileContainer />
+        </View>
+      </View>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    marginVertical: 10,
+    marginHorizontal: 15,
+  },
+});
 
 export default ProfileScreen;
