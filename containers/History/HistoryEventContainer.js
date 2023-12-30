0c1883671599427002d3762c67fceccfd00496b0
Ivan Sholana <ivansholana@gmail.com>
2023-12-30 12:29:35 +0700

    feat : add some conditional rendering for event screen

--- containers/History/HistoryEventContainer.js
+++ containers/History/HistoryEventContainer.js
@@ -1,34 +1,46 @@
+import React from "react";
 import { useSelector } from "react-redux";
+
 import EventCardComponent from "../../components/Card/EventCard";
-import { FlatList } from "react-native";
+import { View, Text, FlatList } from "react-native";
 
 const HistoryEventContainer = ({ navigation }) => {
   const eventData = useSelector((state) => state.event.event);
   const userdata = useSelector((state) => state.login.name);
   const status = useSelector((state) => state.login.status);
 
+  const isAdmin = status === "Admin";
+  const isNoPendingRequests = !eventData.length == 0;
+
+  const filteredData = !isAdmin
+    ? eventData.filter((e) => e.namaPeminjam === userdata)
+    : eventData.filter((e) => e.status === "Diajukan");
+
+  const renderEventCard = ({ item }) => (
+    <EventCardComponent
+      eventDate={item.tanggalAwal}
+      eventName={item.namaAcara}
+      status={item.status}
+      onPress={() =>
+        navigation.navigate(
+          status === "Admin" ? "AdminResponse" : "DetailEvent",
+          { data: item }
+        )
+      }
+    />
+  );
+
   return (
     <>
-      <FlatList
-        data={
-          status !== "Admin"
-            ? eventData.filter((e) => e.namaPeminjam == userdata)
-            : eventData.filter((e) => e.status == "Diajukan")
-        }
-        renderItem={({ item }) => (
-          <EventCardComponent
-            eventDate={item.tanggalAwal}
-            eventName={item.namaAcara}
-            status={item.status}
-            onPress={() =>
-              navigation.navigate(
-                status == "Admin" ? "AdminResponse" : "DetailEvent",
-                { data: item }
-              )
-            }
-          />
-        )}
-      />
+      {isNoPendingRequests ? (
+        <View>
+          <Text style={{ textAlign: "center" }}>
+            Tidak Ada Pengajuan Reservasi
+          </Text>
+        </View>
+      ) : (
+        <FlatList data={filteredData} renderItem={renderEventCard} />
+      )}
     </>
   );
 };
